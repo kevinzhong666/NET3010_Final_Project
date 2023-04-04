47c6ba31cab8c71c9ca5aebd56c31a1b8bce9ce9
Ethan Franks <92701789+efranks27@users.noreply.github.com>
2023-04-04 17:50:32 -0400

    Uploaded about us and our team pages

---
+++ ourteam.php
@@ -0,0 +1,37 @@
+<?php include("head.php")?>
+        <body>
+        <?php include("header.php")?>
+        <?php include("nav.php")?>
+            <div class="useArea">
+                <table border="0">
+
+                <h1>Meet the Team!</h1>
+            <!--my short biography in paragrpah format-->
+            <p>My name is Olivia Taylor and I am currently a 3rd year Network Technology student at 
+                Carleton University and I am this group project's frontend designer and co-team lead.
+                You can contact me with any concerns at: </p>
+                    <li> Carleton email: <a href="mailto:oliviataylor@cmail.carleton.ca">oliviataylor@cmail.carleton.ca</a></li>
+                    <li> Algonquin email: <a href="mailto:tayl0649@algonquinlive.com">tayl0649@algonquinlive.com</a></li> </p>
+            <p>My name is Ethan Franks and I am currently a 3rd year Network Technology student at 
+                Carleton University and I am this group project's co-frontend developper and co-team lead.</p>
+                <p>I can be reached at:</p>
+                <li> Carleton email: <a href="mailto:oliviataylor@cmail.carleton.ca">oliviataylor@cmail.carleton.ca</a></li>
+                <li> Algonquin email: <a href="mailto:tayl0649@algonquinlive.com">tayl0649@algonquinlive.com</a></li> </p>
+            <p>My name is Kevin Zhong and I am currently a 3rd year Network Technology student at 
+                Carleton University and I am this group project's co-frontend developper.</p> 
+            <p>My name is Tarnvir Hans and I am currently a 3rd year Network Technology student at 
+                Carleton University and I am this group project's co-backend designer.</p>
+            <p>My name is Adeeb Fahim and I am currently a 3rd year Network Technology student at 
+                Carleton University and I am this group project's co-backend developper.</p>
+            <!-- placing my headshot to the right-->
+            <img src="images/Olivia.jpg" style="float:right; width:50%; height: 50%">
+            <img src="images/Ethan.jpg" style="float:right; width:50%; height: 50%">
+            <img src="images/Kevin.jpg" style="float:right; width:50%; height: 50%">
+            <img src="images/Tarnvir.jpg" style="float:right; width:50%; height: 50%">
+            <img src="images/Adeeb.jpg" style="float:right; width:50%; height: 50%">
+            
+            </ul>
+            <?php include("footer.php")?>
+        </body>
+    </html>
+</DOCTYPE>
