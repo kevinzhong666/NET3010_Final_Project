6980621a43c9e40ffaba017f68d55b3566a8c791
Ethan Franks <92701789+efranks27@users.noreply.github.com>
2023-04-04 18:22:36 -0400

    started edits on ourteam

--- ourteam.php
+++ ourteam.php
@@ -4,32 +4,46 @@
         <?php include("nav.php")?>
             <div class="useArea">
                 <table border="0">
+            <h2 class="text">Meet the Team!</h2>
+            <ul>
+                <section id="liv">
+                    <img src="images/Olivia.png" alt="Olivia image" style="width:100px; height:100px; float:left; padding: 10px 10px 10px 0px">
+                    <p class="text">My name is Olivia Taylor and I am currently a 3rd year Network Technology student at 
+                                    Carleton University and I am this group project's frontend designer and co-team lead.
+                                    You can contact me with any concerns at: </p>
 
-                <h1>Meet the Team!</h1>
-            <!--my short biography in paragrpah format-->
-            <p>My name is Olivia Taylor and I am currently a 3rd year Network Technology student at 
-                Carleton University and I am this group project's frontend designer and co-team lead.
-                You can contact me with any concerns at: </p>
-                    <li> Carleton email: <a href="mailto:oliviataylor@cmail.carleton.ca">oliviataylor@cmail.carleton.ca</a></li>
-                    <li> Algonquin email: <a href="mailto:tayl0649@algonquinlive.com">tayl0649@algonquinlive.com</a></li> </p>
-            <p>My name is Ethan Franks and I am currently a 3rd year Network Technology student at 
-                Carleton University and I am this group project's co-frontend developper and co-team lead.</p>
-                <p>I can be reached at:</p>
-                <li> Carleton email: <a href="mailto:oliviataylor@cmail.carleton.ca">oliviataylor@cmail.carleton.ca</a></li>
-                <li> Algonquin email: <a href="mailto:tayl0649@algonquinlive.com">tayl0649@algonquinlive.com</a></li> </p>
-            <p>My name is Kevin Zhong and I am currently a 3rd year Network Technology student at 
-                Carleton University and I am this group project's co-frontend developper.</p> 
-            <p>My name is Tarnvir Hans and I am currently a 3rd year Network Technology student at 
-                Carleton University and I am this group project's co-backend designer.</p>
-            <p>My name is Adeeb Fahim and I am currently a 3rd year Network Technology student at 
-                Carleton University and I am this group project's co-backend developper.</p>
-            <!-- placing my headshot to the right-->
-            <img src="images/Olivia.jpg" style="float:right; width:50%; height: 50%">
-            <img src="images/Ethan.jpg" style="float:right; width:50%; height: 50%">
-            <img src="images/Kevin.jpg" style="float:right; width:50%; height: 50%">
-            <img src="images/Tarnvir.jpg" style="float:right; width:50%; height: 50%">
-            <img src="images/Adeeb.jpg" style="float:right; width:50%; height: 50%">
-            
+                    <li class="emailbullets"> Carleton email: <a href="mailto:oliviataylor@cmail.carleton.ca">oliviataylor@cmail.carleton.ca</a></li>
+                    <li class="emailbullets"> Algonquin email: <a href="mailto:tayl0649@algonquinlive.com">tayl0649@algonquinlive.com</a></li> </p>
+                </section>
+                
+                <section id="ethan">
+                    <img src="images/Ethan.png" alt="Ethan image" style="width:100px; height:100px; float:left; padding: 10px 10px 10px 0px">
+                    <p class="text">My name is Ethan Franks and I am currently a 3rd year Network Technology student at 
+                                    Carleton University and I am this group project's co-frontend developper and co-team lead.
+                    <br>
+                    I can be reached at:</p>
+
+                    <li class="emailbullets"> Carleton email: <a href="mailto:ethanfranks@cmail.carleton.ca">ethanfranks@cmail.carleton.ca</a></li>
+                    <li class="emailbullets"> Algonquin email: <a href="mailto:fran0526@algonquinlive.com">fran0526@algonquinlive.com</a></li> </p>
+                </section>
+                
+                <section id="kevin">
+                <img src="images/Kevin.png" alt="Kevin image" style="width:100px; height:100px; float:left; padding: 10px 10px 10px 0px">
+                <p class="text">My name is Kevin Zhong and I am currently a 3rd year Network Technology student at 
+                                Carleton University and I am this group project's co-frontend developper.</p> 
+                </section>
+                
+                <section id="tarnvir">
+                <img src="images/Tarnvir.png" alt="Tarnvir image" style="width:100px; height:100px; float:left; padding: 10px 10px 10px 0px">
+                <p class="text">My name is Tarnvir Hans and I am currently a 3rd year Network Technology student at 
+                                Carleton University and I am this group project's co-backend designer.</p>
+                </section>
+
+                <section id="adeeb">
+                <img src="images/Adeeb.png" alt="Adeeb image" style="width:100px; height:100px; float:left; padding: 10px 10px 10px 0px">
+                <p class="text">My name is Adeeb Fahim and I am currently a 3rd year Network Technology student at 
+                                Carleton University and I am this group project's co-backend designer.</p>
+                </section>
             </ul>
             <?php include("footer.php")?>
         </body>
