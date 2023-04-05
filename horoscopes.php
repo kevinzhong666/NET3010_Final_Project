04efa82a1a9778959b583c6853347940efc0c8db
Ethan Franks <92701789+efranks27@users.noreply.github.com>
2023-04-04 23:47:57 -0400

    added horoscopes.php
    extremely basic starter. Still needs api and api design.

---
+++ horoscopes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="HOROSCOPE API"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="style.css" />
+    <title>Weather Hub</title>
+  </head>
+
+  <body>
+  
+  <?php include ('header.php'); ?>
+
+  <?php include ('nav.php'); ?>
+
+    <h1> Today's Horoscope for you, John.</h1>
+
+    <!--Horoscope API divs will be here-->
+    
+    
+    <script src="horoscopes.js"></script>
+    <?php include ('footer.php'); ?>
+  </body>
+</html>
