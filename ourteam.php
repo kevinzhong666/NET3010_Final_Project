ea4bfa2b6ce01dcbe9559e89c721c800a261fc51
Liv22-T <93879625+Liv22-T@users.noreply.github.com>
2023-04-12 18:32:28 -0400

    added emails

--- ourteam.php
+++ ourteam.php
@@ -24,10 +24,7 @@
                     <p class="text">My name is Ethan Franks and I am currently a 3rd year Network Technology student at Carleton University and I am this group project's co-frontend 
                                     developer and co-team lead. I worked closely with Kevin to implement Olivia’s design and overall look of the website to each page’s front end. I converted all of our original 
                                     HTML to PHP files including snippets (head.php, nav.php, etc.) and code that displays different content based on whether the user was logged in or not. 
-                                    I also helped connect the back-end developers’ database to the forms that users see and fill out so that their usernames and info would be acessed/stored properly.
-
-                    <br>
-                    I can be reached at:</p>
+                                    I also helped connect the back-end developers’ database to the forms that users see and fill out so that their usernames and info would be acessed/stored properly.</p>
 
                     <li class="emailbullets"> Carleton email: <a href="mailto:ethanfranks@cmail.carleton.ca">ethanfranks@cmail.carleton.ca</a></li>
                     <li class="emailbullets"> Algonquin email: <a href="mailto:fran0526@algonquinlive.com">fran0526@algonquinlive.com</a></li> </p>
@@ -38,6 +35,9 @@
                 <p class="text">My name is Kevin Zhong and I am currently a 3rd year Network Technology student at Carleton University and I am this group project's co-frontend developer. 
                                 I collaborated with Ethan on the project's frontend and focused on integrating the weather API with the CSS and JavaScript. Additionally, I assisted Adeeb 
                                 and Tarnvir with debugging the SQL database on the backend.</p> 
+                        
+                    <li class="emailbullets"> Carleton email: <a href="mailto:kevinzhong@cmail.carleton.ca">kevinzhong@cmail.carleton.ca</a></li>
+                    <li class="emailbullets"> Algonquin email: <a href="mailto:zhon0033@algonquinlive.com">zhon0033@algonquinlive.com</a></li> </p>
                 </section>
                 
                 <section id="tarnvir">
@@ -45,6 +45,9 @@
                 <p class="text">My name is Tarnvir Hans and I am currently a 3rd year Network Technology student at Carleton University and I am this group project's co-backend developer.
                                 I worked with Adeeb to set up the database, and the Sign Up page. We worked on linking the database to the Sign up form and making sure the data was stored 
                                 in the correct location. Lastly we worked on the Login page where we had to retrieve the information form the database.</p>
+                        
+                    <li class="emailbullets"> Carleton email: <a href="mailto:tarnvirhans@cmail.carleton.ca">tarnvirhans@cmail.carleton.ca</a></li>
+                    <li class="emailbullets"> Algonquin email: <a href="mailto:hans0204@algonquinlive.com">hans0204@algonquinlive.com</a></li> </p>
                 </section>
 
                 <section id="adeeb">
@@ -52,6 +55,9 @@
                 <p class="text">My name is Adeeb Fahim and I am currently a 3rd year Network Technology student at Carleton University and I am this group project's co-backend developer. 
                                 I worked closely with Tarnvir to help set up the database, and helped set up the form for the Sign Up page. Within the sign up page, I helped set up the link 
                                 between it and the database. Finally, I aided the team in working on retrieving information from the SQL Database and displaying it thru the login page.</p>
+                    
+                    <li class="emailbullets"> Carleton email: <a href="mailto:adeebfahim@cmail.carleton.ca">adeebfahim@cmail.carleton.ca</a></li>
+                    <li class="emailbullets"> Algonquin email: <a href="mailto:fahi0016 @algonquinlive.com">fahi0016 @algonquinlive.com</a></li> </p>
                 </section>
             </ul>
             <?php include("footer.php")?>
